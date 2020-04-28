1ea8980f1c7e6bf6844a8993e31eaa8f1a092fa9
kannam12 <kannam12@github.com>
2020-04-28 17:47:30 +0200

    połowa!

--- JPWP-Zadania-Ola-Sylwia/Zad3/app/src/main/java/com/example/loginy/MainActivity.java
+++ JPWP-Zadania-Ola-Sylwia/Zad3/app/src/main/java/com/example/loginy/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.loginy;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void showDialog(View view) {
+
+        //LayoutInflater inflater = getActivity().getLayoutInflater();
+        //View vie = inflater.inflate(R.layout.okienko, null);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("LOGOWANIE");
+        alert.setMessage("Zaloguj się:");
+
+
+
+        alert.setPositiveButton("LOG IN", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "OK-pomyślne logowanie", Toast.LENGTH_SHORT).show();
+
+                //String login = loginTxt.getText().toString();
+                //String password = passwordTxt.getText().toString();
+
+                //if poprawne
+
+                Intent intent = new Intent(MainActivity.this, WitajActivity.class);
+                startActivity(intent);
+
+                //else
+            }
+        });
+        alert.setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Wychodzimy w takim razie", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.create().show();
+    }
+
+
 }
---
+++ JPWP-Zadania-Ola-Sylwia/Zad3/app/src/main/java/com/example/loginy/WitajActivity.java
@@ -0,0 +1,14 @@
+package com.example.loginy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WitajActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_witaj);
+    }
+}
